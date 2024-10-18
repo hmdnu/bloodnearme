ef6e3034e3829a6427126cfe62dd32cf36ac202d
Nerots <daniuyan71@gmail.com>
2024-10-18 18:36:23 +0700

    chore : delete undefined import

--- client/app/components/index.ts
+++ client/app/components/index.ts
@@ -2,8 +2,7 @@ import Nav from "./Nav";
 import HospitalCard from "./cards/HospitalCard";
 import CommunityCard from "./cards/CommunityCard";
 import Search from "./forms/SearchBar";
-import Filter from "./forms/Filter";
 import BloodTable from "./BloodTable";
 import ContactBox from "./ContactBox";
 
-export { Nav, HospitalCard, CommunityCard, Search, Filter, BloodTable, ContactBox };
+export { Nav, HospitalCard, CommunityCard, Search, BloodTable, ContactBox };
